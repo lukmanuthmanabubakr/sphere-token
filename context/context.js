c327a255180e88ecb8d58b6df4b0512edb331eff
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-02-13 00:03:43 +0100

    first commit

---
+++ context/context.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import toast from "react-hot-toast";
+import JSBI from "jsbi";
+import Web3Modal from "web3modal";
+
+//INTERNAL IMPORT UNISWAP
+import { SwapRouter } from "@uniswap/universal-router-sdk";
+import {
+  TradeType,
+  Ether,
+  Token,
+  CurrencyAmount,
+  Percent,
+} from "@uniswap/sdk-core";
+
+import { Trade as V2Trade } from "@uniswap/v2-sdk";
+import {
+  Pool,
+  nearestUsableTick,
+  TickMath,
+  TICK_SPACINGS,
+  FeeAmount,
+  Trade as V3Trade,
+  Route as RouteV3,
+} from "@uniswap/v3-sdk";
+
+import { MixedRouteTrade, Trade as RouterTrade } from "@uniswap/router-sdk";
+import IUniswapV3Poolcfrom from "@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json";
+
+//INTERNAL IMPORT
+import { ERC20_ABI, web3Provider, CONNECTING_CONTRACT } from "./constants";
+import { shortenAddress, parseErrorMsg } from "../utils/index";
+
+
+export const CONTEXT = React.createContext();
+
+export const PROVIDER = ({children}) => {
+    const TOKEN_SWAP = "TOKEN SWAP DAPP";
+    const [loader, setLoader] = useState(false);
+    const [address, setaddress] = useState("");
+    const [chainId, setchainId] = useState()
+}
