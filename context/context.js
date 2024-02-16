324922a745db6fc9605ab34f4a0344a2944517b5
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-02-16 16:16:09 +0100

    good-job

--- context/context.js
+++ context/context.js
@@ -98,7 +98,6 @@ export const PROVIDER = ({ children }) => {
       IUniswapV3Pool.abi,
       provider
     );
-    
 
     let liquidity = await contract.liquidity();
 
@@ -126,8 +125,6 @@ export const PROVIDER = ({ children }) => {
       },
     ]);
   }
-
- 
 
   // SWAP_OPTION FUNCTION INTERNAL
   function swapOptions(options) {
@@ -179,77 +176,77 @@ export const PROVIDER = ({ children }) => {
     console.log(token_1, token_2, swapInputAmount);
 
     // try {
-    console.log("CALLING ME _________________SWAP");
-    // const _inputAmount = 1;
-    const provider = await web3Provider();
+    //   console.log("CALLING ME _________________SWAP");
+    //   // const _inputAmount = 1;
+    //   const provider = await web3Provider();
 
-    // const network = await provider.getNetwork();
-    const ETHER = Ether.onChain(token_1.chainId);
-    // const ETHER = Ether.onChain(1);
+    //   // const network = await provider.getNetwork();
+    //   const ETHER = Ether.onChain(token_1.chainID);
+    //   // const ETHER = Ether.onChain(1);
 
-    //   // TOKEN CONTRACT
-    const tokenAddress1 = await CONNECTING_CONTRACT(token_1.address);
-    const tokenAddress2 = await CONNECTING_CONTRACT(token_2.address);
+    //   //   // TOKEN CONTRACT
+    //   const tokenAddress1 = await CONNECTING_CONTRACT(token_1.address);
+    //   const tokenAddress2 = await CONNECTING_CONTRACT(token_2.address);
 
-    //   // TOKEN DETAILS
-    const TOKEN_A = new Token(
-      tokenAddress1.chainId,
-      tokenAddress1.address,
-      tokenAddress1.decimals,
-      tokenAddress1.symbol,
-      tokenAddress1.name
-    );
+    //   //   // TOKEN DETAILS
+    //   const TOKEN_A = new Token(
+    //     tokenAddress1.chainId,
+    //     tokenAddress1.address,
+    //     tokenAddress1.decimals,
+    //     tokenAddress1.symbol,
+    //     tokenAddress1.name
+    //   );
 
-    const TOKEN_B = new Token(
-      tokenAddress2.chainId,
-      tokenAddress2.address,
-      tokenAddress2.decimals,
-      tokenAddress2.symbol,
-      tokenAddress2.name
-    );
+    //   const TOKEN_B = new Token(
+    //     tokenAddress2.chainId,
+    //     tokenAddress2.address,
+    //     tokenAddress2.decimals,
+    //     tokenAddress2.symbol,
+    //     tokenAddress2.name
+    //   );
 
-    const WETH_USDC_V3 = await getPool(
-      TOKEN_A,
-      TOKEN_B,
-      FeeAmount.MEDIUM,
-      provider
-    );
+    //   const WETH_USDC_V3 = await getPool(
+    //     TOKEN_A,
+    //     TOKEN_B,
+    //     FeeAmount.MEDIUM,
+    //     provider
+    //   );
 
-    const inputEther = ethers.utils.parseEther(swapInputAmount).toString();
+    //   const inputEther = ethers.utils.parseEther(swapInputAmount).toString();
 
-  console.log("WETH_USDC_V3:", WETH_USDC_V3);
-  console.log("ETHER:", ETHER);
-  console.log("TOKEN_B:", TOKEN_B);
+    //   console.log("WETH_USDC_V3:", WETH_USDC_V3);
+    //   console.log("ETHER:", ETHER);
+    //   console.log("TOKEN_B:", TOKEN_B);
 
-  const trade = await V3Trade.fromRoute(
-    new RouteV3([WETH_USDC_V3], ETHER, TOKEN_B),
-    CurrencyAmount.fromRawAmount(ETHER, inputEther),
-    TradeType.EXACT_INPUT
-  );
+    //   const trade = await V3Trade.fromRoute(
+    //     new RouteV3([WETH_USDC_V3], ETHER, TOKEN_B),
+    //     CurrencyAmount.fromRawAmount(ETHER, inputEther),
+    //     TradeType.EXACT_INPUT
+    //   );
 
-  const routerTrade = buildTrade([trade]);
+    //   const routerTrade = buildTrade([trade]);
 
-  const opts = swapOptions({});
+    //   const opts = swapOptions({});
 
-  const params = SwapRouter.swapERC20CallParameters(routerTrade, opts);
+    //   const params = SwapRouter.swapERC20CallParameters(routerTrade, opts);
 
-  console.log(WETH_USDC_V3);
-  console.log(trade);
-  console.log(routerTrade);
-  console.log(opts);
-  console.log(params);
+    //   console.log(WETH_USDC_V3);
+    //   console.log(trade);
+    //   console.log(routerTrade);
+    //   console.log(opts);
+    //   console.log(params);
 
-  let ethBalance;
-  let tokenA;
-  let tokenB;
+    //   let ethBalance;
+    //   let tokenA;
+    //   let tokenB;
 
-  ethBalance = await provider.getBalance(RECIPIENT);
-  tokenA = await tokenAddress1.balance;
-  tokenB = await tokenAddress2.balance;  
-  console.log("------------------BEFORE");
-  console.log("EthBalance:", ethers.utils.formatUnits(ethBalance, 18));
-  console.log("tokenA:", tokenA);
-  console.log("tokenB:", tokenB);
+    //   ethBalance = await provider.getBalance(RECIPIENT);
+    //   tokenA = await tokenAddress1.balance;
+    //   tokenB = await tokenAddress2.balance;
+    //   console.log("------------------BEFORE");
+    //   console.log("EthBalance:", ethers.utils.formatUnits(ethBalance, 18));
+    //   console.log("tokenA:", tokenA);
+    //   console.log("tokenB:", tokenB);
 
     //   const tx = await signerTransaction({
     //     data: params.calldata,
@@ -277,11 +274,7 @@ export const PROVIDER = ({ children }) => {
     //   notifyError(errorMsg);
     //   console.log(error);
     // }
-
   };
-
-
-
 
   return (
     <CONTEXT.Provider
