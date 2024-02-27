b1323bf53fec25a554278891444eb12dd333b3f8
dev_dimeji✔ <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2024-02-27 23:58:48 +0100

    Update Lock.js

--- test/Lock.js
+++ test/Lock.js
@@ -6,8 +6,6 @@ const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
 describe("Lock", function () {
-  
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployOneYearLockFixture() {
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     const ONE_GWEI = 1_000_000_000;
